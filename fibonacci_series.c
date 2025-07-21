1be3b44cd7507b3f8c491c44d04091f1770ae664
DerhasatGoyary <et24bthcs028@kzu.ac.in>
2025-07-21 14:58:57 +0530

    Update fibonacci_series.c

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -1,12 +1,10 @@
-// Program 13: Find the Fibonacci series up to N terms
+// Find the Fibonacci series up to N terms
 #include <stdio.h>
 
 int main() {
     int n, a = 0, b = 1, next;
-    // Input number of terms
     printf("Enter the number of terms: ");
     scanf("%d", &n);
-    // Print Fibonacci series
     printf("Fibonacci Series: ");
     for (int i = 0; i < n; i++) {
         printf("%d ", a);
